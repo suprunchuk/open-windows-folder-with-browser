0e05bff6d7adf2cfdfa278e3a3a9711f3ec697a1
Pasha Suprunchuk <suprunchuk@internet.ru>
2024-02-02 02:05:14 +0300

    FIX: readme, server, added gif

--- server.js
+++ server.js
@@ -1,23 +1,45 @@
 const express = require("express");
+const path = require("path");
 const { exec } = require("child_process");
-const app = express();
-const port = 3000;
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Открывает папку в проводнике через браузер
+function openFolderInExplorer(folderPath) {
+    switch (process.platform) {
+        case "darwin":
+            exec(`open "${folderPath}"`, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    return;
+                }
+            });
+            break;
+        case "win32":
+            exec(`explorer "${folderPath}"`, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    return;
+                }
+            });
+            break;
+        default:
+            console.log("Unsupported platform");
+    }
+}
+
+// Маршрут для открытия папки
 app.get("/open-folder", (req, res) => {
     const folderPath = req.query.path;
-    const command = `explorer.exe /select,"${folderPath}"`;
-
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Ошибка при выполнении команды: ${error}`);
-            res.status(500).send({ error: "Ошибка при открытии папки" });
-            return;
-        }
-        console.log(`Папка открыта: ${folderPath}`);
-        res.send("Папка успешно открыта");
-    });
+    if (!folderPath) {
+        res.status(400).send("Не указан путь к папке");
+        return;
+    }
+    openFolderInExplorer(folderPath);
+    res.send("Папка открыта в проводнике");
 });
 
-app.listen(port, () => {
-    console.log(`Сервер запущен на http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
 });
