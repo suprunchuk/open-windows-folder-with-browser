6ffa1bd28d48ad3077df8c4cf32629b64216a1fc
Pasha Suprunchuk <suprunchuk@internet.ru>
2024-02-02 09:18:41 +0300

    Fixed bugs

--- server.js
+++ server.js
@@ -5,30 +5,29 @@ const { exec } = require("child_process");
 const app = express();
 const PORT = 3030;
 
-// Открывает папку в проводнике через браузер
 function openFolderInExplorer(folderPath) {
+    let command;
     switch (process.platform) {
         case "darwin":
-            exec(`open "${folderPath}"`, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    return;
-                }
-            });
+            command = `open "${folderPath}"`;
             break;
         case "win32":
-            exec(`explorer "${folderPath}"`, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    return;
-                }
-            });
+            command = `start explorer "${folderPath}"`;
             break;
         default:
             console.log("Unsupported platform");
+            return;
     }
+
+    exec(command, { shell: true }, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+    });
 }
 
+//CORS
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
@@ -36,15 +35,22 @@ app.use((req, res, next) => {
     next();
 });
 
-
-// Маршрут для открытия папки
 app.get("/open-folder", (req, res) => {
     const folderPath = req.query.path;
     if (!folderPath) {
         res.status(400).send("Не указан путь к папке");
         return;
     }
-    openFolderInExplorer(folderPath);
+
+    const result = openFolderInExplorer(folderPath);
+    if (result instanceof Error) {
+        res.status(500).send("Ошибка при открытии папки");
+        return;
+    } else if (result === "Unsupported platform") {
+        res.status(500).send("Платформа не поддерживается");
+        return;
+    }
+
     res.send("Папка открыта в проводнике");
 });
 
